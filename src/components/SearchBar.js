36939b02c8a753cabb710e9107d0f97604599570
Nick Boober <nboober@terpmail.umd.edu>
2020-02-18 16:30:20 -0500

    updated some text in the searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ class SearchBar extends React.Component{
     render(){
         return(
             <div>
-                Search Our Course By Name Here <br/>
+                Search Our Courses By Name Here <br/>
                 <input type="text" onChange={(event)=>this.props.search(event)}/>
             </div>
         )
