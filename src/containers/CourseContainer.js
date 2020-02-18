cb9d94699dc47ad9d4511d2eadab707097ec3f35
Nick Boober <nboober@terpmail.umd.edu>
2020-02-18 16:28:24 -0500

    implemented error handling in the course container if no search results were found

--- src/containers/CourseContainer.js
+++ src/containers/CourseContainer.js
@@ -5,10 +5,10 @@ class CourseContainer extends React.Component{
     render(){
         return(
             <div>
-                {this.props.courses ? this.props.courses.map(course=>{
+                {this.props.courses ? this.props.courses.length === 0 ? "No Search results Found" : this.props.courses.map(course=>{
                     return <Course key={course.id} course={course} view={this.props.view}/>
                 })
-            : null}
+            : "hello"}
             </div>
         )
     }
