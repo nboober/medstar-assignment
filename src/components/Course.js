fea2834d06b78f63c3599773fe97e60990ec04a1
Nick Boober <nboober@terpmail.umd.edu>
2020-02-18 22:21:55 -0500

    added lorem epsum text to the course to fill up more space

--- src/components/Course.js
+++ src/components/Course.js
@@ -8,7 +8,7 @@ class Course extends React.Component{
                     <div className="card text-white bg-info mb-3" style={{maxWidth: "18rem"}}>
                         <div className="card-body">
                             <h5 className="card-title">{this.props.course.title}</h5>
-                            <p className="card-text">{this.props.course.description}</p>
+                            <p className="card-text">{this.props.course.description.substring(0, 200)}</p>
                         </div>
                     </div>
             </Link>
--- src/courses/courses.js
+++ src/courses/courses.js
@@ -3,14 +3,14 @@ let courses = [
         id: 1,
         "title": "Course 1",
         "filter": ["hard", "cool course", "fun course"],
-        "description": "This is course 1. This is about courses."
+        "description": "Himenaeos lacinia libero ante mollis imperdiet porttitor erat euismod suspendisse bibendum. Dictumst class laoreet sagittis sem cubilia condimentum aptent dolor? Sit dapibus, habitant et. Aliquet faucibus vehicula a senectus aenean netus posuere adipiscing nisl adipiscing laoreet maecenas. Vivamus posuere vulputate tristique cubilia scelerisque porttitor tempus erat. Inceptos blandit gravida, arcu sodales. Urna proin viverra consectetur torquent elit nascetur maecenas duis lectus, semper natoque dignissim. Fringilla sed blandit vitae felis non lobortis per justo conubia porta. Parturient nascetur in duis, consequat non neque tellus sem! Penatibus cum tincidunt at! Mattis scelerisque molestie at blandit. Tempus urna malesuada velit vivamus libero eu, molestie scelerisque lobortis consectetur. Consectetur facilisis molestie convallis. Montes cursus himenaeos arcu lacus metus sit parturient dictum conubia nullam condimentum. Cum, condimentum felis ante ante libero iaculis class quam phasellus. Mi habitasse sodales netus tellus praesent adipiscing euismod nam ad scelerisque. Magna, gravida class fermentum sem a ornare aenean curabitur nisl ullamcorper quisque. Dis, et leo sagittis quisque habitant lacinia. Blandit, fringilla torquent in tristique pellentesque. Ipsum eleifend facilisi senectus viverra blandit nisl sem turpis. Habitant placerat fames?"
     }
     ,
     {
         id: 2,
         "title": "Course 2",
         "filter": ["easy", "fun course"],
-        "description": "This is course 2. This is more about courses."
+        "description": "Himenaeos lacinia libero ante mollis imperdiet porttitor erat euismod suspendisse bibendum. Dictumst class laoreet sagittis sem cubilia condimentum aptent dolor? Sit dapibus, habitant et. Aliquet faucibus vehicula a senectus aenean netus posuere adipiscing nisl adipiscing laoreet maecenas. Vivamus posuere vulputate tristique cubilia scelerisque porttitor tempus erat. Inceptos blandit gravida, arcu sodales. Urna proin viverra consectetur torquent elit nascetur maecenas duis lectus, semper natoque dignissim. Fringilla sed blandit vitae felis non lobortis per justo conubia porta. Parturient nascetur in duis, consequat non neque tellus sem! Penatibus cum tincidunt at! Mattis scelerisque molestie at blandit. Tempus urna malesuada velit vivamus libero eu, molestie scelerisque lobortis consectetur. Consectetur facilisis molestie convallis. Montes cursus himenaeos arcu lacus metus sit parturient dictum conubia nullam condimentum. Cum, condimentum felis ante ante libero iaculis class quam phasellus. Mi habitasse sodales netus tellus praesent adipiscing euismod nam ad scelerisque. Magna, gravida class fermentum sem a ornare aenean curabitur nisl ullamcorper quisque. Dis, et leo sagittis quisque habitant lacinia. Blandit, fringilla torquent in tristique pellentesque. Ipsum eleifend facilisi senectus viverra blandit nisl sem turpis. Habitant placerat fames?"
 
     }
     ,
@@ -18,7 +18,7 @@ let courses = [
         id: 3,
         "title": "Course 3",
         "filter": ["easy", "fun course"],
-        "description": "This is course 3. This is more about courses."
+        "description": "Himenaeos lacinia libero ante mollis imperdiet porttitor erat euismod suspendisse bibendum. Dictumst class laoreet sagittis sem cubilia condimentum aptent dolor? Sit dapibus, habitant et. Aliquet faucibus vehicula a senectus aenean netus posuere adipiscing nisl adipiscing laoreet maecenas. Vivamus posuere vulputate tristique cubilia scelerisque porttitor tempus erat. Inceptos blandit gravida, arcu sodales. Urna proin viverra consectetur torquent elit nascetur maecenas duis lectus, semper natoque dignissim. Fringilla sed blandit vitae felis non lobortis per justo conubia porta. Parturient nascetur in duis, consequat non neque tellus sem! Penatibus cum tincidunt at! Mattis scelerisque molestie at blandit. Tempus urna malesuada velit vivamus libero eu, molestie scelerisque lobortis consectetur. Consectetur facilisis molestie convallis. Montes cursus himenaeos arcu lacus metus sit parturient dictum conubia nullam condimentum. Cum, condimentum felis ante ante libero iaculis class quam phasellus. Mi habitasse sodales netus tellus praesent adipiscing euismod nam ad scelerisque. Magna, gravida class fermentum sem a ornare aenean curabitur nisl ullamcorper quisque. Dis, et leo sagittis quisque habitant lacinia. Blandit, fringilla torquent in tristique pellentesque. Ipsum eleifend facilisi senectus viverra blandit nisl sem turpis. Habitant placerat fames?"
 
     }
     ,
@@ -26,7 +26,7 @@ let courses = [
         id: 4,
         "title": "Course 4",
         "filter": ["medium", "ok course"],
-        "description": "This is course 4. This is more about courses."
+        "description": "Himenaeos lacinia libero ante mollis imperdiet porttitor erat euismod suspendisse bibendum. Dictumst class laoreet sagittis sem cubilia condimentum aptent dolor? Sit dapibus, habitant et. Aliquet faucibus vehicula a senectus aenean netus posuere adipiscing nisl adipiscing laoreet maecenas. Vivamus posuere vulputate tristique cubilia scelerisque porttitor tempus erat. Inceptos blandit gravida, arcu sodales. Urna proin viverra consectetur torquent elit nascetur maecenas duis lectus, semper natoque dignissim. Fringilla sed blandit vitae felis non lobortis per justo conubia porta. Parturient nascetur in duis, consequat non neque tellus sem! Penatibus cum tincidunt at! Mattis scelerisque molestie at blandit. Tempus urna malesuada velit vivamus libero eu, molestie scelerisque lobortis consectetur. Consectetur facilisis molestie convallis. Montes cursus himenaeos arcu lacus metus sit parturient dictum conubia nullam condimentum. Cum, condimentum felis ante ante libero iaculis class quam phasellus. Mi habitasse sodales netus tellus praesent adipiscing euismod nam ad scelerisque. Magna, gravida class fermentum sem a ornare aenean curabitur nisl ullamcorper quisque. Dis, et leo sagittis quisque habitant lacinia. Blandit, fringilla torquent in tristique pellentesque. Ipsum eleifend facilisi senectus viverra blandit nisl sem turpis. Habitant placerat fames?"
 
     }
     ,
@@ -34,7 +34,7 @@ let courses = [
         id: 5,
         "title": "Course 5",
         "filter": ["hard", "tough course"],
-        "description": "This is course 5. This is more about courses."
+        "description": "Himenaeos lacinia libero ante mollis imperdiet porttitor erat euismod suspendisse bibendum. Dictumst class laoreet sagittis sem cubilia condimentum aptent dolor? Sit dapibus, habitant et. Aliquet faucibus vehicula a senectus aenean netus posuere adipiscing nisl adipiscing laoreet maecenas. Vivamus posuere vulputate tristique cubilia scelerisque porttitor tempus erat. Inceptos blandit gravida, arcu sodales. Urna proin viverra consectetur torquent elit nascetur maecenas duis lectus, semper natoque dignissim. Fringilla sed blandit vitae felis non lobortis per justo conubia porta. Parturient nascetur in duis, consequat non neque tellus sem! Penatibus cum tincidunt at! Mattis scelerisque molestie at blandit. Tempus urna malesuada velit vivamus libero eu, molestie scelerisque lobortis consectetur. Consectetur facilisis molestie convallis. Montes cursus himenaeos arcu lacus metus sit parturient dictum conubia nullam condimentum. Cum, condimentum felis ante ante libero iaculis class quam phasellus. Mi habitasse sodales netus tellus praesent adipiscing euismod nam ad scelerisque. Magna, gravida class fermentum sem a ornare aenean curabitur nisl ullamcorper quisque. Dis, et leo sagittis quisque habitant lacinia. Blandit, fringilla torquent in tristique pellentesque. Ipsum eleifend facilisi senectus viverra blandit nisl sem turpis. Habitant placerat fames?"
 
     }
     ,
@@ -42,7 +42,7 @@ let courses = [
         id: 6,
         "title": "Course 6",
         "filter": ["easy", "beginner course"],
-        "description": "This is course 6. This is more about courses."
+        "description": "Himenaeos lacinia libero ante mollis imperdiet porttitor erat euismod suspendisse bibendum. Dictumst class laoreet sagittis sem cubilia condimentum aptent dolor? Sit dapibus, habitant et. Aliquet faucibus vehicula a senectus aenean netus posuere adipiscing nisl adipiscing laoreet maecenas. Vivamus posuere vulputate tristique cubilia scelerisque porttitor tempus erat. Inceptos blandit gravida, arcu sodales. Urna proin viverra consectetur torquent elit nascetur maecenas duis lectus, semper natoque dignissim. Fringilla sed blandit vitae felis non lobortis per justo conubia porta. Parturient nascetur in duis, consequat non neque tellus sem! Penatibus cum tincidunt at! Mattis scelerisque molestie at blandit. Tempus urna malesuada velit vivamus libero eu, molestie scelerisque lobortis consectetur. Consectetur facilisis molestie convallis. Montes cursus himenaeos arcu lacus metus sit parturient dictum conubia nullam condimentum. Cum, condimentum felis ante ante libero iaculis class quam phasellus. Mi habitasse sodales netus tellus praesent adipiscing euismod nam ad scelerisque. Magna, gravida class fermentum sem a ornare aenean curabitur nisl ullamcorper quisque. Dis, et leo sagittis quisque habitant lacinia. Blandit, fringilla torquent in tristique pellentesque. Ipsum eleifend facilisi senectus viverra blandit nisl sem turpis. Habitant placerat fames?"
 
     }
 ]
