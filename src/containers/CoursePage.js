e12ee898bd1de8a3a9aedf85110965e756ac2807
Nick Boober <nboober@terpmail.umd.edu>
2020-02-20 14:28:47 -0500

    centered the video element on desktop screens

--- src/containers/CoursePage.js
+++ src/containers/CoursePage.js
@@ -38,12 +38,14 @@ class CoursePage extends React.Component{
                         </p>
 
                         {this.props.info.courseType.video !== "n/a" ? 
-                        
+
+                        <div className="videoContainer">
                             <video className="video" controls>
                                 <source src="movie.mp4" type="video/mp4" />
                                 <source src="movie.ogg" type="video/ogg" />
                                 Your browser does not support the video tag.
                             </video> 
+                        </div>
 
                         : null}
 
