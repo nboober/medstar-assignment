70c20e9d7ea89fee9c5d6cc8fd2fd2b13b0220ce
Nick Boober <nboober@terpmail.umd.edu>
2020-02-20 14:19:53 -0500

    updated the styling of the book in the the course page component

--- src/containers/CoursePage.js
+++ src/containers/CoursePage.js
@@ -33,24 +33,29 @@ class CoursePage extends React.Component{
                         </h2>
                         <hr/>
                         <h4>Description:</h4>
-                        <p>
+                        <p className="description">
                             {this.props.info.description}
                         </p>
 
                         {this.props.info.courseType.video !== "n/a" ? 
                         
-                            <video className="video" width="100%" height="auto" controls>
+                            <video className="video" controls>
                                 <source src="movie.mp4" type="video/mp4" />
                                 <source src="movie.ogg" type="video/ogg" />
                                 Your browser does not support the video tag.
                             </video> 
+
                         : null}
 
                         {this.props.info.courseType.book !== "n/a" ? 
                         <div className="book">
+
                             <p>{this.props.info.courseType.book[this.state.page]}</p>
-                            <button onClick={() => this.pageTurner(-1)}>Previous Page</button>
-                            <button onClick={() => this.pageTurner(1)}>Next Page</button>
+
+                            <div className="pageTurnerButtons">
+                                <button onClick={() => this.pageTurner(-1)}>Previous Page</button>
+                                <button onClick={() => this.pageTurner(1)}>Next Page</button>
+                            </div>
                         </div>
                         :null}
 
