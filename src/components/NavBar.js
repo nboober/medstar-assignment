db43c2d252929540f17880833a4cc3bf3c3fe92d
Nick Boober <nboober@terpmail.umd.edu>
2020-02-22 11:22:53 -0500

    updated the styling for the navbar dropdown to be on the right for desktop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,19 +9,19 @@ class NavBar extends React.Component{
                 <img src="https://www.medstarhealth.org/content/themes/medstarhealth-specialty-parent/assets/img/logo.png" alt="medstar logo"/>
               </Link>
               <div class="" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto" >
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Options
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Login</a>
-                    <a class="dropdown-item" href="#">Register</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Contact Us</a>
-                  </div>
-                </li>
-              </ul>
+                <ul class="navbar-nav mr-auto" >
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Options Menu
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Login</a>
+                      <a class="dropdown-item" href="#">Register</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Contact Us</a>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </nav>
         )
